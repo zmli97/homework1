f89986dcfc6a9c61b4a3c1289a82dd95b8d45491
li <1932764605@qq.com>
2019-05-29 12:15:00 +0800

    12:14

---
+++ src/main/java/com/cet/homework/service/UserService.java
@@ -0,0 +1,16 @@
+package com.cet.homework.service;
+import com.cet.homework.entity.User;
+import com.cet.homework.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    public User getUser(String number) {
+        return userRepository.find(number);
+    }
+}
