a39f200e68e23873cbc6a1e592c89f2af35cb9d7
noone6 <1831779018@qq.com>
2019-05-29 16:00:04 +0800

    实体类 考试信息

--- src/main/java/com/cet/homework/entity/ExamInfo.java
+++ src/main/java/com/cet/homework/entity/ExamInfo.java
@@ -17,18 +17,17 @@ public class ExamInfo {
     public static final int EXAMINFO_NEEDCOUNT = 1;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int examInfoId;
+    private int id;
     private LocalDateTime beginTime;   /*起始时间*/
     private LocalDateTime endTime;    /*结束时间*/
     private String classRome;         /*地点*/
     private String className; /*课程*/
     // 在没有声明时默认为1
     private int needCount =1 ;  /*所需人数*/
-    private int teacherId;   /*教师编号*/
 
     @ManyToOne(fetch = FetchType.LAZY)
     private User teacher;
     public ExamInfo(int id) {
-        this.examInfoId= id;
+        this.id= id;
     }
 }
