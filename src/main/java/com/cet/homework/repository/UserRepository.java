5b4ca9315e37b8f437eb244e147f911e70b2e357
ShortHair32 <1044919284@qq.com>
2019-05-29 15:37:16 +0800

    User持久操作

--- src/main/java/com/cet/homework/repository/UserRepository.java
+++ src/main/java/com/cet/homework/repository/UserRepository.java
@@ -1,15 +1,15 @@
 package com.cet.homework.repository;
 
 import com.cet.homework.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Repository
-@Transactional
-public interface UserRepository extends JpaRepository<User, Integer> {
+public class UserRepository {
     /**
      * 获取指定ID的用户
      * @param id
@@ -17,4 +17,10 @@ public interface UserRepository extends JpaRepository<User, Integer> {
      */
     @Query("SELECT u FROM User u WHERE u.id=:id")
     User find(@Param("id") int id);
+
+    @PersistenceContext
+    private EntityManager em;
+
+
+
 }
